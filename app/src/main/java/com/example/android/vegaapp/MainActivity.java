17266987d9bd0f4f63a3bc5a6769a653c35c207e
iu-u <42092791+iu-u@users.noreply.github.com>
2020-05-26 00:20:58 +0200

    Added buttons to switch from activity

--- app/src/main/java/com/example/android/vegaapp/MainActivity.java
+++ app/src/main/java/com/example/android/vegaapp/MainActivity.java
@@ -2,9 +2,16 @@ package com.example.android.vegaapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.android.vegaapp.R;
+import com.example.android.vegaapp.activities.LoginActivity;
+import com.example.android.vegaapp.activities.ProfileActivity;
+import com.example.android.vegaapp.activities.RecipeActivity;
+import com.example.android.vegaapp.activities.RecipeDetailActivity;
+import com.example.android.vegaapp.activities.RegisterActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,5 +19,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void loginActivity(View view){
+        Intent intent= new Intent(MainActivity.this, LoginActivity.class);
+        startActivity(intent);
+    }
+
+    public void profileActivity(View view){
+        Intent intent= new Intent(MainActivity.this, ProfileActivity.class);
+        startActivity(intent);
+    }
+    public void recipeActivity(View view){
+        Intent intent= new Intent(MainActivity.this, RecipeActivity.class);
+        startActivity(intent);
+    }
+    public void recipeDetailActivity(View view){
+        Intent intent= new Intent(MainActivity.this, RecipeDetailActivity.class);
+        startActivity(intent);
+    }
+    public void registerActivity(View view){
+        Intent intent= new Intent(MainActivity.this, RegisterActivity.class);
+        startActivity(intent);
     }
 }
