e36354170d4ad9654052c7232b8779b5378449f5
afrash <afrash.ramjit@hotmail.nl>
2020-05-26 15:34:45 +0200

    made ReadWriteActivityDatabaseTest class for database testing

---
+++ app/src/main/java/com/example/android/vegaapp/activities/ReadWriteActivityDatabaseTest.java
@@ -0,0 +1,16 @@
+package com.example.android.vegaapp.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.android.vegaapp.R;
+
+public class ReadWriteActivityDatabaseTest extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_read_write_database_test);
+    }
+}
