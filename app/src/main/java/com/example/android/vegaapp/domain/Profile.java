7aea2312a861f65524e60a3b0a238e943da92e19
2angelo3 <45686008+2angelo3@users.noreply.github.com>
2020-05-26 17:39:40 +0200

    Add Profile class to domain Package.

---
+++ app/src/main/java/com/example/android/vegaapp/domain/Profile.java
@@ -0,0 +1,23 @@
+package com.example.android.vegaapp.domain;
+
+public class Profile {
+        private String email;
+        private String pictureLink;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPictureLink() {
+        return pictureLink;
+    }
+
+    public void setPictureLink(String pictureLink) {
+        this.pictureLink = pictureLink;
+    }
+}
+
