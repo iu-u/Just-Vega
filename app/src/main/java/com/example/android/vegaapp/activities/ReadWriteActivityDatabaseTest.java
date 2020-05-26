3ac72114e94e3feb03d88470fab6f7fda83dc687
afrash <afrash.ramjit@hotmail.nl>
2020-05-26 15:37:30 +0200

    added dependecy for FirebaseDatabase and made read and write methods

--- app/src/main/java/com/example/android/vegaapp/activities/ReadWriteActivityDatabaseTest.java
+++ app/src/main/java/com/example/android/vegaapp/activities/ReadWriteActivityDatabaseTest.java
@@ -3,14 +3,61 @@ package com.example.android.vegaapp.activities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.inputmethod.EditorInfo;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.example.android.vegaapp.R;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class ReadWriteActivityDatabaseTest extends AppCompatActivity {
+
+    private static String TAG = ReadWriteActivityDatabaseTest.class.getName();
+
+    TextView write;
+    FirebaseDatabase database;
+    DatabaseReference myRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_read_write_database_test);
+        database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("test");
+        readFromDatabase();
+
+        write = (TextView) findViewById(R.id.editText2);
+
     }
+
+    public void writeToDatabase(){
+
+        myRef.setValue("Helllo world");
+    }
+
+    public void readFromDatabase(){
+// Read from the database
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                String value = dataSnapshot.getValue(String.class);
+                Log.d(TAG, "Value is: " + value);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                Log.w(TAG, "Failed to read value.", error.toException());
+            }
+        });
+    }
+
+
 }
