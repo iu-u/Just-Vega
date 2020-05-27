f67f02e4b6a515fbc89ffa06ef69062bd3d70913
Thirza van Hemert <cahemert@avans.nl>
2020-05-27 09:21:31 +0200

    Clean up main activity

--- app/src/main/java/com/example/android/vegaapp/MainActivity.java
+++ app/src/main/java/com/example/android/vegaapp/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.android.vegaapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -20,6 +21,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 
     public void loginActivity(View view){
