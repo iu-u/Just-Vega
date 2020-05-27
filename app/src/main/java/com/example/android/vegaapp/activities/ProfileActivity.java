aca53a17a2fb75a0d62adb8e971249b26f3c7225
afrash <afrash.ramjit@hotmail.nl>
2020-05-27 21:53:48 +0200

    Made logout function in ProfileActivity

--- app/src/main/java/com/example/android/vegaapp/activities/ProfileActivity.java
+++ app/src/main/java/com/example/android/vegaapp/activities/ProfileActivity.java
@@ -1,15 +1,27 @@
 package com.example.android.vegaapp.activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
+import com.example.android.vegaapp.MainActivity;
 import com.example.android.vegaapp.R;
+import com.firebase.ui.auth.AuthUI;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
 
 public class ProfileActivity extends AppCompatActivity {
+
+    Button btn_sign_out;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,7 +29,31 @@ public class ProfileActivity extends AppCompatActivity {
         setContentView(R.layout.activity_profile);
         Toolbar mToolbar = (Toolbar) findViewById(R.id.tool_bar);
         setSupportActionBar(mToolbar);
+
+        btn_sign_out = (Button)findViewById(R.id.logoutButton);
+        btn_sign_out.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                AuthUI.getInstance().signOut(ProfileActivity.this)
+                        .addOnCompleteListener(new OnCompleteListener<Void>() {
+                            @Override
+                            public void onComplete(@NonNull Task<Void> task) {
+                                Intent intent= new Intent(ProfileActivity.this, LoginActivity.class);
+                                startActivity(intent);
+                            }
+                        }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(ProfileActivity.this, ""+e.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+        });
+
     }
+
+
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -25,4 +61,7 @@ public class ProfileActivity extends AppCompatActivity {
         inflater.inflate(R.menu.main_menu, menu);
         return true;
     }
+
 }
+
+
--- app/src/main/java/com/example/android/vegaapp/activities/RecipeActivity.java
+++ app/src/main/java/com/example/android/vegaapp/activities/RecipeActivity.java
@@ -28,26 +28,26 @@ public class RecipeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recipe);
 
-        btn_sign_out = (Button)findViewById(R.id.btn_sign_out);
-        btn_sign_out.setOnClickListener(new View.OnClickListener(){
-
-            @Override
-            public void onClick(View v) {
-                AuthUI.getInstance().signOut(RecipeActivity.this)
-                        .addOnCompleteListener(new OnCompleteListener<Void>() {
-                            @Override
-                            public void onComplete(@NonNull Task<Void> task) {
-                                Intent intent= new Intent(RecipeActivity.this, LoginActivity.class);
-                                startActivity(intent);
-                            }
-                        }).addOnFailureListener(new OnFailureListener() {
-                    @Override
-                    public void onFailure(@NonNull Exception e) {
-                        Toast.makeText(RecipeActivity.this, ""+e.getMessage(), Toast.LENGTH_SHORT).show();
-                    }
-                });
-            }
-        });
+//        btn_sign_out = (Button)findViewById(R.id.btn_sign_out);
+//        btn_sign_out.setOnClickListener(new View.OnClickListener(){
+//
+//            @Override
+//            public void onClick(View v) {
+//                AuthUI.getInstance().signOut(RecipeActivity.this)
+//                        .addOnCompleteListener(new OnCompleteListener<Void>() {
+//                            @Override
+//                            public void onComplete(@NonNull Task<Void> task) {
+//                                Intent intent= new Intent(RecipeActivity.this, LoginActivity.class);
+//                                startActivity(intent);
+//                            }
+//                        }).addOnFailureListener(new OnFailureListener() {
+//                    @Override
+//                    public void onFailure(@NonNull Exception e) {
+//                        Toast.makeText(RecipeActivity.this, ""+e.getMessage(), Toast.LENGTH_SHORT).show();
+//                    }
+//                });
+//            }
+//        });
         Toolbar mToolbar = (Toolbar) findViewById(R.id.tool_bar);
         setSupportActionBar(mToolbar);
     }
