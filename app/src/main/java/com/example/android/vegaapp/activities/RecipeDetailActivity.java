6be0d66e2ae6f09f07b8360823f1cf0271167da1
afrash <afrash.ramjit@hotmail.nl>
2020-05-28 13:54:32 +0200

    Toolbar fixed now goes to profile if clicked on icon or "Profile"

--- app/src/main/java/com/example/android/vegaapp/activities/RecipeDetailActivity.java
+++ app/src/main/java/com/example/android/vegaapp/activities/RecipeDetailActivity.java
@@ -3,9 +3,11 @@ package com.example.android.vegaapp.activities;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.View;
 
 import com.example.android.vegaapp.R;
 
@@ -17,6 +19,19 @@ public class RecipeDetailActivity extends AppCompatActivity {
         setContentView(R.layout.activity_recipe_detail);
         Toolbar mToolbar = (Toolbar) findViewById(R.id.tool_bar);
         setSupportActionBar(mToolbar);
+    }
+
+    //click on toolbar to go to profile
+    public void goToProfile(View view){
+        Intent intent= new Intent(RecipeDetailActivity.this, ProfileActivity.class);
+        startActivity(intent);
+    }
+
+
+
+    public void recipeActivity(View view){
+        Intent intent= new Intent(RecipeDetailActivity.this, RecipeActivity.class);
+        startActivity(intent);
     }
 
     @Override
