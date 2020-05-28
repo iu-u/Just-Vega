f278e4cbd431f82a9c577f3198c2f5a77119fbbd
2angelo3 <45686008+2angelo3@users.noreply.github.com>
2020-05-28 14:14:33 +0200

    Fix layout activity_profile.xml

--- app/src/main/java/com/example/android/vegaapp/activities/ProfileActivity.java
+++ app/src/main/java/com/example/android/vegaapp/activities/ProfileActivity.java
@@ -1,9 +1,5 @@
 package com.example.android.vegaapp.activities;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
@@ -13,12 +9,10 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.example.android.vegaapp.MainActivity;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
 import com.example.android.vegaapp.R;
-import com.firebase.ui.auth.AuthUI;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
