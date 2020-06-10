7da81c59d2217488c07343757299753097d214a0
Thirza van Hemert <cahemert@avans.nl>
2020-06-10 14:05:50 +0200

    Add functionality filter on multiple items

--- app/src/main/java/com/example/android/vegaapp/adapters/RecipeAdapter.java
+++ app/src/main/java/com/example/android/vegaapp/adapters/RecipeAdapter.java
@@ -104,7 +104,6 @@ public class RecipeAdapter  extends RecyclerView.Adapter<RecipeAdapter.ViewHolde
         @Override
         protected void publishResults(CharSequence constraint, FilterResults results) {
             Log.i(TAG, "publishResults called");
-            mRecipes.clear();
             mRecipes.addAll((List)results.values);
 
             notifyDataSetChanged();
