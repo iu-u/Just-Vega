5662b198073d3cf967e59efdb7d38c6094543e3b
Thirza van Hemert <cahemert@avans.nl>
2020-06-15 22:23:32 +0200

    Continued Layout RecipePreparationMethodActivity

--- app/src/main/java/com/example/android/vegaapp/activities/RecipepreperationMethodActivity.java
+++ app/src/main/java/com/example/android/vegaapp/activities/RecipepreperationMethodActivity.java
@@ -1,11 +1,14 @@
 package com.example.android.vegaapp.activities;
 
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
@@ -28,6 +31,8 @@ public class RecipepreperationMethodActivity extends AppCompatActivity {
     private TextView mRecipeName;
     private TextView mAllergies;
     private TextView mPreparation;
+    private LinearLayout mLinearLayout;
+    private TextView mToFName;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -41,7 +46,6 @@ public class RecipepreperationMethodActivity extends AppCompatActivity {
         String title = intent.getExtras().getString("name");
         String category = intent.getExtras().getString("category");
         String image = intent.getExtras().getString("image");
-        String preparation = intent.getExtras().getString("preparation");
         ArrayList<String> allergieList = intent.getStringArrayListExtra("allergies");
         HashMap<String, List<String>> mPrepMap = (HashMap<String, List<String>>)intent.getSerializableExtra("prepMap");
 
@@ -51,7 +55,8 @@ public class RecipepreperationMethodActivity extends AppCompatActivity {
         mCategory = findViewById(R.id.category);
         mRecipeName = findViewById(R.id.recipeName);
         mAllergies = findViewById(R.id.allergies);
-        mPreparation = findViewById(R.id.textView_preparation);
+        mLinearLayout = findViewById(R.id.prepLayout);
+
 
         mGoToRecipe.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -78,9 +83,8 @@ public class RecipepreperationMethodActivity extends AppCompatActivity {
             sb.append(s).append(", ");
         }
         mAllergies.setText(sb.toString());
-        Log.d(TAG, "preparationMethod: " + preparation);
-        mPreparation.setText(preparation);
 
+        setElements(mPrepMap);
 
 //        for(String s: mPrepMap.keySet()){
 //            System.out.println("current key: " + s);
@@ -89,5 +93,103 @@ public class RecipepreperationMethodActivity extends AppCompatActivity {
 //            }
 //        }
 
+    }
+
+    public void setElements(HashMap<String, List<String>> map){
+        int currentNumber = 1;
+
+        for(String s: map.keySet()){
+            Log.d(TAG, "key: " + s);
+
+            //Make Horizontal LinearLayout
+            LinearLayout wrapperLayout = new LinearLayout(this);
+            wrapperLayout.setOrientation(LinearLayout.HORIZONTAL);
+
+            //Make TextView currentNumber for wrapperLayout
+            TextView currentToF = new TextView(this);
+            currentToF.setTextColor(getResources().getColor(R.color.colorTextGreen));
+            currentToF.setText(currentNumber + ".");
+            currentToF.setGravity(Gravity.RIGHT);
+            currentNumber++;
+
+            //Make vertical LinearLayout for Preparation TextViews
+            LinearLayout prepLayout = new LinearLayout(this);
+            prepLayout.setBackgroundColor(getResources().getColor(R.color.colorBlack));
+            prepLayout.setOrientation(LinearLayout.VERTICAL);
+
+
+            //Make Title TextView for vertical LinearLayout
+            TextView tof = new TextView(this);
+            tof.setTextColor(getResources().getColor(R.color.colorTextWhite));
+            tof.setTypeface(null, Typeface.BOLD);
+            tof.setText(s);
+
+            //Make Preperation Instructions TextView for vertical LinearLayout
+            TextView prep = new TextView(this);
+            prep.setTextColor(getResources().getColor(R.color.colorTextLightGrey));
+            StringBuilder sb = new StringBuilder("");
+            for(String string: map.get(s)){
+                sb.append(string).append('\n');
+            }
+            Log.d(TAG, "preparation string: " + sb.toString());
+            prep.setText(sb.toString());
+
+            //Make LinearLayout for button for wrapperLayout
+            LinearLayout buttonLayout = new LinearLayout(this);
+            buttonLayout.setMinimumWidth(30);
+            buttonLayout.setMinimumHeight(30);
+
+
+            //Make button for LinearLayout
+            Button checkButton = new Button(this);
+            checkButton.setBackground(getResources().getDrawable(R.drawable.circle_fave_icon));
+            checkButton.setHeight(20);
+            checkButton.setWidth(20);
+
+            buttonLayout.addView(checkButton);
+
+            //Add Textviews to prepLayout
+            prepLayout.addView(tof);
+            prepLayout.addView(prep);
+
+            LinearLayout.LayoutParams paramsWrapper =new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.MATCH_PARENT);
+            paramsWrapper.setMargins(0,40,20,20);
+            wrapperLayout.setLayoutParams(paramsWrapper);
+
+            LinearLayout.LayoutParams params =new LinearLayout.LayoutParams(0, LinearLayout.LayoutParams.MATCH_PARENT);
+            params.weight = 0.76f;
+            params.setMargins(20,0,20,0);
+            prepLayout.setLayoutParams(params);
+
+            LinearLayout.LayoutParams paramsSmall =new LinearLayout.LayoutParams(0, LinearLayout.LayoutParams.MATCH_PARENT);
+            paramsSmall.weight = 0.12f;
+            paramsSmall.setMargins(0,0,10,0);
+            currentToF.setLayoutParams(paramsSmall);
+
+            LinearLayout.LayoutParams paramsButton =new LinearLayout.LayoutParams(0, LinearLayout.LayoutParams.MATCH_PARENT);
+            paramsButton.weight = 0.12f;
+            buttonLayout.setLayoutParams(paramsButton);
+
+//            LinearLayout border = new LinearLayout(this);
+//            border.setBackgroundColor(getResources().getColor(R.color.colorVagueGreen));
+//            border.setMinimumHeight(1);
+
+            View border = new View(this);
+            border.setBackgroundColor(getResources().getColor(R.color.colorVagueGreen));
+            border.setMinimumHeight(1);
+
+            LinearLayout.LayoutParams paramsBorder =new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.MATCH_PARENT);
+            border.setLayoutParams(paramsBorder);
+
+            wrapperLayout.setWeightSum(1f);
+
+            //Add Elements to wrapperLayout
+            wrapperLayout.addView(currentToF);
+            wrapperLayout.addView(prepLayout);
+            wrapperLayout.addView(buttonLayout);
+
+            mLinearLayout.addView(wrapperLayout);
+            mLinearLayout.addView(border);
+        }
     }
 }
