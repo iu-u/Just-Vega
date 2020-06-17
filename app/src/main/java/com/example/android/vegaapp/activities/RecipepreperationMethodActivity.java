c76d409bc5117a47b3c86b98c1ab5d82adbb2d9e
Thirza van Hemert <cahemert@avans.nl>
2020-06-17 18:46:28 +0200

    Add timer

--- app/src/main/java/com/example/android/vegaapp/activities/RecipepreperationMethodActivity.java
+++ app/src/main/java/com/example/android/vegaapp/activities/RecipepreperationMethodActivity.java
@@ -1,19 +1,30 @@
 package com.example.android.vegaapp.activities;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.animation.ValueAnimator;
 import android.content.Intent;
 import android.graphics.Typeface;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.os.Handler;
 import android.util.Log;
+import android.view.ContextThemeWrapper;
 import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.ProgressBar;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.content.ContextCompat;
 
 import com.bumptech.glide.Glide;
 import com.example.android.vegaapp.R;
@@ -21,6 +32,9 @@ import com.example.android.vegaapp.R;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Locale;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class RecipepreperationMethodActivity extends AppCompatActivity {
     private static String TAG = RecipepreperationMethodActivity.class.getName();
@@ -33,6 +47,20 @@ public class RecipepreperationMethodActivity extends AppCompatActivity {
     private TextView mPreparation;
     private LinearLayout mLinearLayout;
     private TextView mToFName;
+    private ProgressBar progressBar;
+    RelativeLayout relativeLayout;
+    private int progressStatus = 0;
+    private Handler handler = new Handler();
+    ValueAnimator animator;
+
+    //Timer
+    private long mStartTimeInMillis;
+    private boolean mTimeRunning;
+    private long mTimeLeftInMillis;
+    private long mEndTime;
+    private CountDownTimer mCountdownTimer;
+    TextView textView;
+    private long i = 0;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -56,6 +84,8 @@ public class RecipepreperationMethodActivity extends AppCompatActivity {
         mRecipeName = findViewById(R.id.recipeName);
         mAllergies = findViewById(R.id.allergies);
         mLinearLayout = findViewById(R.id.prepLayout);
+        progressBar = findViewById(R.id.progress_bar);
+        relativeLayout = findViewById(R.id.timerLayout);
 
 
         mGoToRecipe.setOnClickListener(new View.OnClickListener() {
@@ -93,6 +123,7 @@ public class RecipepreperationMethodActivity extends AppCompatActivity {
         int currentNumber = 1;
 
         for(String s: map.keySet()){
+            boolean timer = false;
             Log.d(TAG, "key: " + s);
 
             //Make Horizontal LinearLayout
@@ -124,6 +155,11 @@ public class RecipepreperationMethodActivity extends AppCompatActivity {
             StringBuilder sb = new StringBuilder("");
             for(String string: map.get(s)){
                 sb.append(string).append('\n');
+                if(string.contains("minuten")){
+                    getAmountOfMinutes(string);
+                    timer = true;
+                    Log.d(TAG, "StartAmountMillis: " + mStartTimeInMillis);
+                }
             }
             prep.setText(sb.toString());
 
@@ -145,6 +181,7 @@ public class RecipepreperationMethodActivity extends AppCompatActivity {
             prepLayout.addView(tof);
             prepLayout.addView(prep);
 
+            //Add params to layouts
             LinearLayout.LayoutParams paramsWrapper =new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.MATCH_PARENT);
             paramsWrapper.setMargins(0,40,20,20);
             wrapperLayout.setLayoutParams(paramsWrapper);
@@ -163,10 +200,6 @@ public class RecipepreperationMethodActivity extends AppCompatActivity {
             paramsButton.weight = 0.12f;
             buttonLayout.setLayoutParams(paramsButton);
 
-//            LinearLayout border = new LinearLayout(this);
-//            border.setBackgroundColor(getResources().getColor(R.color.colorVagueGreen));
-//            border.setMinimumHeight(1);
-
             View border = new View(this);
             border.setBackgroundColor(getResources().getColor(R.color.colorVagueGreen));
             border.setMinimumHeight(1);
@@ -182,7 +215,121 @@ public class RecipepreperationMethodActivity extends AppCompatActivity {
             wrapperLayout.addView(buttonLayout);
 
             mLinearLayout.addView(wrapperLayout);
+            if(timer){
+                mLinearLayout.addView(getTimerLayout());
+            }
             mLinearLayout.addView(border);
         }
     }
+
+    public View getTimerLayout(){
+        LinearLayout lnWrapper = new LinearLayout(this);
+        lnWrapper.setMinimumHeight(100);
+        LinearLayout lnLeft = new LinearLayout(this);
+        LinearLayout lnRight = new LinearLayout(this);
+
+        LinearLayout.LayoutParams paramsLeft =new LinearLayout.LayoutParams(0, LinearLayout.LayoutParams.MATCH_PARENT);
+        paramsLeft.weight = 0.25f;
+        lnLeft.setLayoutParams(paramsLeft);
+
+        LinearLayout.LayoutParams paramsRight =new LinearLayout.LayoutParams(0, LinearLayout.LayoutParams.MATCH_PARENT);
+        paramsRight.weight = 0.75f;
+        lnRight.setLayoutParams(paramsRight);
+
+
+        //RelativeLayout
+        RelativeLayout timerLayout = new RelativeLayout(this);
+        LinearLayout.LayoutParams paramsTimer =new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.MATCH_PARENT);
+        timerLayout.setLayoutParams(paramsTimer);
+        timerLayout.setBackgroundColor(getResources().getColor(R.color.fui_transparent));
+
+        //ProgressBar
+        final ProgressBar progressBar = new ProgressBar(this, null, android.R.attr.progressBarStyleHorizontal);
+        progressBar.setLayoutParams(paramsTimer);
+        progressBar.isIndeterminate();
+        progressBar.setProgressDrawable(ContextCompat.getDrawable(this, R.drawable.custom_progress_bar));
+
+        textView = new TextView(this);
+        int minutes = (int) (mStartTimeInMillis / 1000) / 60;
+        int seconds = (int) (mStartTimeInMillis / 1000) % 60;
+
+        final String timeLeftFormatted = String.format(Locale.getDefault(),"%02d:%02d", minutes, seconds);
+
+        textView.setText(timeLeftFormatted);
+        textView.setTextColor(getResources().getColor(R.color.colorTextGreen));
+        textView.setGravity(Gravity.CENTER);
+
+        final Button playButton = new Button(this);
+        playButton.setBackground(ContextCompat.getDrawable(this, R.drawable.ic_play_arrow_green));
+        animator = ValueAnimator.ofInt(0, progressBar.getMax());
+        animator.setDuration(mStartTimeInMillis);
+
+        playButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if(mTimeRunning){
+                    Toast.makeText(RecipepreperationMethodActivity.this, "Timer reset", Toast.LENGTH_SHORT).show();
+                    animator.cancel();
+                    mTimeRunning = false;
+                    playButton.setBackground(ContextCompat.getDrawable(RecipepreperationMethodActivity.this, R.drawable.ic_play_arrow_green));
+                    textView.setText(timeLeftFormatted);
+                } else{
+                    Toast.makeText(RecipepreperationMethodActivity.this, "Timer started", Toast.LENGTH_SHORT).show();
+                    animator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+                        @Override
+                        public void onAnimationUpdate(ValueAnimator animation){
+                            progressBar.setProgress((Integer)animation.getAnimatedValue());
+                        }
+                    });
+                    animator.start();
+                    textView.setText("Started");
+                    mTimeRunning = true;
+                    playButton.setBackground(ContextCompat.getDrawable(RecipepreperationMethodActivity.this, R.drawable.ic_close_green_small));
+                }
+            }
+        });
+
+        LinearLayout lnTime = new LinearLayout(this);
+
+        LinearLayout.LayoutParams paramsButton =new LinearLayout.LayoutParams(0, LinearLayout.LayoutParams.MATCH_PARENT);
+        paramsButton.weight = 0.30f;
+        playButton.setLayoutParams(paramsButton);
+
+        LinearLayout.LayoutParams paramsText =new LinearLayout.LayoutParams(0, LinearLayout.LayoutParams.MATCH_PARENT);
+        paramsText.weight = 0.70f;
+        textView.setLayoutParams(paramsText);
+
+        lnTime.setWeightSum(1f);
+        lnTime.addView(playButton);
+        lnTime.addView(textView);
+
+
+        timerLayout.addView(progressBar);
+        timerLayout.addView(lnTime);
+
+        lnLeft.addView(timerLayout);
+
+        lnWrapper.setOrientation(LinearLayout.HORIZONTAL);
+        lnWrapper.setWeightSum(1f);
+        lnWrapper.addView(lnLeft);
+        lnWrapper.addView(lnRight);
+
+
+        return lnWrapper;
+    }
+
+
+
+    private void getAmountOfMinutes(String s){
+        int minutes = 0;
+        Pattern p = Pattern.compile("(\\w+)\\W+minuten\\W+(\\w+)");
+        Matcher m = p.matcher(s);
+        if(m.find());
+            System.out.printf("'%s', '%s'", m.group(1), m.group(2));
+            minutes = (Integer.parseInt(m.group(1))) * 60000;
+            long l = minutes;
+            mStartTimeInMillis = l;
+    }
+
 }
